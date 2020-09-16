a4fa35685392335c7df4ac39e5522ec56a7dcfbf
Matheus Santos <matheussm301@gmail.com>
2020-09-16 20:40:23 -0300

    feat: Finalize test

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,23 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const checkCustomerExits = await this.customersRepository.findByEmail(
+      email,
+    );
+
+    if (checkCustomerExits) {
+      throw new AppError('Email Already used');
+    }
+
+    const customer = await this.customersRepository.create({ name, email });
+
+    return customer;
   }
 }
 
